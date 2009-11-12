488896431f138703b0b018fa3be68deef9d8b28f
Joshua Davey <josh@joshuadavey.com>
2009-11-11 21:41:56 -0600

    whitespace

--- rails_generators/nifty_scaffold/templates/inherited_resources_controller.rb
+++ rails_generators/nifty_scaffold/templates/inherited_resources_controller.rb
@@ -1,8 +1,8 @@
 class <%= plural_class_name %>Controller < InheritedResources::Base
   actions :<%= controller_actions.join(', :') %>
-	<%- if options[:will_paginate] -%>
-  
-	protected
+  <%- if options[:will_paginate] -%>
+
+  protected
   def collection
     @<%= plural_name %> ||= end_of_association_chain.paginate :page => params[:page], :per_page => (params[:per_page] || 20)
   end
